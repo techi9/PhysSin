0c0c8e91631c3653ccbd1e0b0d8afd2b83a51df3
techi9 <tucha.989@gmail.com>
2021-11-10 00:50:57 +0300

    NEw

--- GameBody.cpp
+++ GameBody.cpp
@@ -1,8 +1,9 @@
 #include "SFML/Graphics.hpp"
 #include "string"
 #include "math.h"
+#include "iostream"
 
-#define gravity 0.0001
+#define gravity -0.0001
 #define sizeX 256
 #define sizeY 128
 using namespace std;
@@ -14,13 +15,12 @@ public:
     float weight = 1;
     Vector2f speed;
     Vector2f pos;
-    sf::Vector2f m_w;
+    float m_w = 0;
     float r_m = 4.0/3.0 * sizeX * sizeY * (sizeX^2 + sizeY^2) * 1; //
     vector<pair<float,float>> collisionPoints;
 
     GameBody(Vector2f startPos, Vector2f startSpeed){
-        m_w.x = 0;
-        m_w.y = 0;
+
 
         speed = startSpeed;
         pos = startPos;
@@ -30,7 +30,7 @@ public:
         m_rect.setPoint(1, Vector2f(sizeX / 2.0, -sizeY / 2.0));
         m_rect.setPoint(2, Vector2f(-sizeX / 2.0, -sizeY / 2.0));
         m_rect.setPoint(3, Vector2f(-sizeX/ 2.0, sizeY / 2.0));
-
+        applyRotation(1);
     }
 
     void applyRotation(float angle){
@@ -43,22 +43,14 @@ public:
 
 
     }
-    sf::Vector2f cross(sf::Vector2f v, sf::Vector2f i){
-        return sf::Vector2f(-i * v.y , i * v.x);
-    }
+
     void applyForce(){}//TODO: implement
-    void bounce(int indexOfVertex){ // r is m_rect.getPoint(indexOfVertex)  n - Normal (0,1)
-        sf::Vector2f n(0,1);
-        sf::Vector2f vp =  speed + cross(m_rect.getPoint(indexOfVertex), m_w);  //?? m_w.x m_w.y
-        float vp_p = vp.x * n.x + vp.y * n.y;
-        float j = -vp_p / (1/weight + (m_rect.getPoint(indexOfVertex).x * n.y - m_rect.getPoint(indexOfVertex).y * n.x) / r_m);
-        sf::Vector2f jn = j*n;
-        speed = speed + jn / weight;
-        m_w = m_w + cross(m_rect.getPoint(indexOfVertex),jn) / r_m;
+    void bounce(int indexOfVertex){
 
     }
     void update(){
         for(int i = 0; i < m_rect.getPointCount(); i++) {
+           // cout<<"Y="<<(pos + m_rect.getPoint(i)).y<<endl;
             if ((pos + m_rect.getPoint(i)).y <= 0) {
                 bounce(i);
             }
@@ -70,6 +62,7 @@ public:
         speed.y += gravity;
         pos.x += speed.x * 0.5;
         pos.y += speed.y * 0.5;
+        applyRotation(m_w);
         m_rect.setPosition(pos.x, pos.y);
     }
 
